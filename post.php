0f66a5d80bdcbb057e887932fcf6408d0ad59216
Jugoslav Djokic <jugoslavng@gmail.com>
2017-05-02 13:11:04 +0200

    midified post.php

--- post.php
+++ post.php
@@ -1,6 +1,6 @@
 <?php include 'includes/header.php'; ?>
     <div class="blog-post">
-        <h2 class="blog-post-title">Sample blog post</h2>
+        <h2 class="blog-post-title">International php Conference 2017</h2>
         <p class="blog-post-meta">January 1, 2014 by <a href="#">Mark</a></p>
 
         <p>Nulla id tempor metus. Aliquam est purus, rhoncus quis venenatis in, pharetra vitae nulla. Cras laoreet purus lacus, at faucibus nisi rhoncus eget. Curabitur mollis, lacus sed lobortis scelerisque, est odio bibendum turpis, sed posuere neque justo rutrum urna. Curabitur sagittis, tortor vel congue volutpat, justo justo maximus tortor, quis sollicitudin tellus ligula a ex. Integer non urna eu elit aliquet venenatis. Sed vestibulum ullamcorper libero eget vulputate. Etiam porta porttitor dui. Suspendisse pulvinar ante felis, ut dapibus nisi gravida sagittis. Cras in ligula gravida, cursus risus sit amet, malesuada nulla. Aliquam id pulvinar dolor. In viverra massa vitae quam gravida, et porta massa accumsan. Curabitur ultricies urna nec augue rhoncus commodo. Etiam et dignissim metus. Praesent vehicula aliquam ex vitae pharetra.</p>

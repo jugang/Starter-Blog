c77353a240f2a3d4b815e51816f3141b741ee724
Jugoslav Djokic <jugoslavng@gmail.com>
2017-05-03 00:09:17 +0200

    Modified files

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
+<?php include 'config/config.php' ?>
+<?php include 'libraries/Database.php' ?>
 <?php include 'includes/header.php'; ?>
+<?php
+    $db = new Database();
+?>
             <div class="blog-post">
                 <h2 class="blog-post-title">Sample blog post</h2>
                 <p class="blog-post-meta">January 1, 2014 by <a href="#">Mark</a></p>
--- libraries/database.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Jugoslav
- * Date: 2.5.2017.
- * Time: 0:02
- */
